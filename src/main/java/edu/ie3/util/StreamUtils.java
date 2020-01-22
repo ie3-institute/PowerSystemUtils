8b478d84a4ab38ff38b36109be0c95765435f62e
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2020-01-22 11:51:49 +0100

    preparations for sonatype/maven central publishing

--- src/main/java/edu/ie3/util/StreamUtils.java
+++ src/main/java/edu/ie3/util/StreamUtils.java
@@ -20,7 +20,7 @@ import java.util.function.Predicate;
  */
 public class StreamUtils {
   /**
-   * Usage: Stream.filter(StreamUtils.distinctByKey(node -> node.getSubnet[0]))
+   * Usage: Stream.filter(StreamUtils.distinctByKey(node to node.getSubnet[0]))
    *
    * @param key The key to filter for
    * @param <T> Type of the Element to filter for
