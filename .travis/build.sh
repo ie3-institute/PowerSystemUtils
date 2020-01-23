fb2862cdea2ecbea645bed1c4dac9f4d33f36b0b
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2020-01-23 14:03:21 +0100

    debug adaptions in build.sh

--- .travis/build.sh
+++ .travis/build.sh
@@ -5,8 +5,13 @@ if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];
 then
     echo "Merge PR into master. Calling all tests + creating reports."
     ./gradlew --refresh-dependencies clean spotlessCheck pmdMain pmdTest spotbugsMain spotbugsTest allTests jacocoTestReport jacocoTestCoverageVerification
-else
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ]
+then
     echo "Building PR $TRAVIS_PULL_REQUEST."
     echo "Skipping integration tests in pull request builds."
-    ./gradlew --refresh-dependencies clean spotlessCheck pmdMain pmdTest spotbugsMain spotbugsTest allTests jacocoTestReport jacocoTestCoverageVerification -x integrationTest
+    ./gradlew --refresh-dependencies clean spotlessCheck pmdMain pmdTest spotbugsMain spotbugsTest allTests jacocoTestReport jacocoTestCoverageVerification
+else
+    echo "Building branch $TRAVIS_BRANCH."
+    echo "Skipping integration tests in pull request builds."
+    ./gradlew --refresh-dependencies clean spotlessCheck pmdMain pmdTest spotbugsMain spotbugsTest allTests jacocoTestReport jacocoTestCoverageVerification
 fi
