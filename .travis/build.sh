c9716ed80ff52b3ef679539a3689ff5d0a28a248
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2020-01-23 13:54:41 +0100

    added sudo: true to travis.yml

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,9 +4,9 @@ chmod u+x gradlew
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];
 then
     echo "Merge PR into master. Calling all tests + creating reports."
-    ./gradlew clean spotlessCheck pmdMain pmdTest spotbugsMain spotbugsTest allTests jacocoTestReport jacocoTestCoverageVerification
+    ./gradlew --refresh-dependencies clean spotlessCheck pmdMain pmdTest spotbugsMain spotbugsTest allTests jacocoTestReport jacocoTestCoverageVerification
 else
     echo "Building PR $TRAVIS_PULL_REQUEST."
     echo "Skipping integration tests in pull request builds."
-    ./gradlew clean spotlessCheck pmdMain pmdTest spotbugsMain spotbugsTest allTests jacocoTestReport jacocoTestCoverageVerification -x integrationTest
+    ./gradlew --refresh-dependencies clean spotlessCheck pmdMain pmdTest spotbugsMain spotbugsTest allTests jacocoTestReport jacocoTestCoverageVerification -x integrationTest
 fi
