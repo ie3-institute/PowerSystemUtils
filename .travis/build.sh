ebeba7c46f22d0dfb03cfe38b321cb5a83618997
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2020-01-23 13:30:54 +0100

    adapted travis.yml to account to prevent duplicate builds

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 
 chmod u+x gradlew
+echo "Merge PR into master. Calling all tests + creating reports."
+./gradlew --refresh-dependencies clean spotlessCheck pmdMain pmdTest spotbugsMain spotbugsTest allTests jacocoTestReport jacocoTestCoverageVerification
+
+chmod u+x gradlew
 echo "Building PR $TRAVIS_PULL_REQUEST."
 echo "Skipping integration tests in pull request builds."
 ./gradlew --refresh-dependencies clean spotlessCheck pmdMain pmdTest spotbugsMain spotbugsTest allTests jacocoTestReport jacocoTestCoverageVerification -x integrationTest
--- .travis/run_on_merge_request.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-chmod u+x gradlew
-echo "Merge PR into master. Calling all tests + creating reports."
-./gradlew --refresh-dependencies clean spotlessCheck pmdMain pmdTest spotbugsMain spotbugsTest allTests jacocoTestReport jacocoTestCoverageVerification
