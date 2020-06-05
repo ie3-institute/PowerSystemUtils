498bf40b4a505bb4c232f7994be2c7d62d6c9004
Kittl, Chris <chris.kittl@tu-dortmund.de>
2020-06-05 13:23:53 +0200

    Enhance test

--- src/test/groovy/edu/ie3/util/quantities/QuantityUtilTest.groovy
+++ src/test/groovy/edu/ie3/util/quantities/QuantityUtilTest.groovy
@@ -5,8 +5,11 @@
  */
 package edu.ie3.util.quantities
 
+import spock.lang.Unroll
+
 import static edu.ie3.util.quantities.PowerSystemUnits.DEGREE_GEOM
 import static edu.ie3.util.quantities.PowerSystemUnits.KILOWATT
+import static edu.ie3.util.quantities.PowerSystemUnits.MEGAWATT
 import static edu.ie3.util.quantities.PowerSystemUnits.KILOWATTHOUR_PER_SQUAREMETRE
 import static tec.uom.se.unit.Units.METRE
 import static tec.uom.se.unit.Units.METRE_PER_SECOND
@@ -41,7 +44,8 @@ class QuantityUtilTest extends Specification {
 		Quantities.getQuantity(30d, METRE)                        || Quantities.getQuantity(30d, METRE) as ComparableQuantity<Length>
 	}
 
-	def "The QuantityUtil calculates absolute considerably equal values correctly"() {
+	@Unroll
+	def "The QuantityUtil calculates absolute considerably equal values correctly (a = #a, b = #b, expected result = #expected)"() {
 		when:
 		def actual = QuantityUtil.considerablyAbsEqual(a, b, 0.001)
 
@@ -51,12 +55,14 @@ class QuantityUtilTest extends Specification {
 		where:
 		a                                               | b                                        || expected
 		Quantities.getQuantity(10d, KILOWATT)           | Quantities.getQuantity(10d, KILOWATT)    || true
+		Quantities.getQuantity(10d, KILOWATT)           | Quantities.getQuantity(0.010, MEGAWATT)  || true
 		Quantities.getQuantity(15.0000001, DEGREE_GEOM) | Quantities.getQuantity(15d, DEGREE_GEOM) || true
 		Quantities.getQuantity(15.1, DEGREE_GEOM)       | Quantities.getQuantity(15d, DEGREE_GEOM) || false
 		Quantities.getQuantity(20d, METRE_PER_SECOND)   | Quantities.getQuantity(10d, KILOWATT)    || false
 	}
 
-	def "The QuantityUtil calculates relative considerably equal values correctly"() {
+	@Unroll
+	def "The QuantityUtil calculates relative considerably equal values correctly (a = #a, b = #b, expected result = #expected)"() {
 		when:
 		def actual = QuantityUtil.considerablyRelEqual(a, b, 0.1)
 
@@ -66,6 +72,7 @@ class QuantityUtilTest extends Specification {
 		where:
 		a                                               | b                                        || expected
 		Quantities.getQuantity(10d, KILOWATT)           | Quantities.getQuantity(10d, KILOWATT)    || true
+		Quantities.getQuantity(10d, KILOWATT)           | Quantities.getQuantity(0.010, MEGAWATT)  || true
 		Quantities.getQuantity(15.0000001, DEGREE_GEOM) | Quantities.getQuantity(15d, DEGREE_GEOM) || true
 		Quantities.getQuantity(15.9, DEGREE_GEOM)       | Quantities.getQuantity(14d, DEGREE_GEOM) || false
 		Quantities.getQuantity(20d, METRE_PER_SECOND)   | Quantities.getQuantity(10d, KILOWATT)    || false
