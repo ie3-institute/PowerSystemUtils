799302229c175051eceaad6f45c81f2c8a29b1d4
Kittl, Chris <chris.kittl@tu-dortmund.de>
2021-04-08 08:28:26 +0200

    Add an enumeration about existing naming strategies

---
+++ src/main/java/edu/ie3/util/naming/NamingConvention.java
@@ -0,0 +1,27 @@
+/*
+ * © 2021. TU Dortmund University,
+ * Institute of Energy Systems, Energy Efficiency and Energy Economics,
+ * Research group Distribution grid planning and operation
+*/
+package edu.ie3.util.naming;
+
+/**
+ * An enumeration of commonly known and used naming conventions according to Wikipedia.
+ *
+ * @see <a href="https://en.wikipedia.org/wiki/Naming_convention_(programming)">this Wikipedia
+ *     article</a>
+ */
+public enum NamingConvention {
+  Flat,
+  UpperFlat,
+  Camel,
+  Pascal,
+  Snake,
+  ScreamingSnake,
+  CamelSnake,
+  PascalSnake,
+  Kebab,
+  Doner,
+  ScreamingKebab,
+  Train
+}
