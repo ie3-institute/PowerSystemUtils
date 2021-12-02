646bd0949b24f7e5fa732c4bd797de5b24f216fb
t-ober <63147366+t-ober@users.noreply.github.com>
2021-12-02 17:04:25 +0100

    rework filters

--- src/main/scala/edu/ie3/util/osm/OsmModel.scala
+++ src/main/scala/edu/ie3/util/osm/OsmModel.scala
@@ -6,8 +6,6 @@
 package edu.ie3.util.osm
 
 import edu.ie3.util.osm.OsmEntities.{ClosedWay, Node, Relation, Way}
-import edu.ie3.util.osm.TagUtils.Keys.{building, highway, landuse}
-import edu.ie3.util.osm.TagUtils.Values.{highWayValues, landUseValues}
 import org.locationtech.jts.geom.Polygon
 
 /** Container class that contains OpenStreetMap data for a specified area.
@@ -30,39 +28,99 @@ case class OsmModel(
 
 object OsmModel {
 
-  /** Extract buildings from a list of ways.
-    *
-    * @param ways
-    *   the ways to extract from
-    * @return
-    *   all was that represent buildings
-    */
-  def extractBuildings(ways: List[Way]): List[ClosedWay] = {
-    ways.collect { case way: ClosedWay if way.tags.contains(building) => way }
+  object KeyUtils {
+    val BUILDING = "building"
+    val HIGHWAY = "highway"
+    val LANDUSE = "landuse"
   }
 
-  /** Extract high ways from a list of ways.
+  /** Convenience method to extract buildings from a list of ways
     *
     * @param ways
     *   the ways to extract from
-    * @return
-    *   all ways that represent highways
+    * @param specificValues
+    *   only match values within the Set when given otherwise match all
     */
-  def extractHighways(ways: List[Way]): List[Way] = {
-    ways.filter(way => way.containsKeyValuePair(highway, highWayValues))
+  def extractBuildings(
+      ways: List[Way],
+      specificValues: Option[Set[String]] = None
+  ): List[ClosedWay] = {
+    extractClosedWays(ways, KeyUtils.BUILDING, specificValues)
   }
 
-  /** Extract land uses from a list of ways
+  /** Convenience method to extract land uses from a list of ways
     *
     * @param ways
     *   the ways to extract from
-    * @return
-    *   all ways that represent land uses
+    * @param specificValues
+    *   only match values within the Set when given otherwise match all
     */
-  def extractLandUses(ways: List[Way]): List[ClosedWay] = {
-    ways.collect {
-      case way: ClosedWay if way.containsKeyValuePair(landuse, landUseValues) =>
-        way
+  def extractLandUses(
+      ways: List[Way],
+      specificValues: Option[Set[String]] = None
+  ): List[ClosedWay] = {
+    extractClosedWays(ways, KeyUtils.LANDUSE, specificValues)
+  }
+
+  /** Extract closed ways via their tags.
+    *
+    * @param ways
+    *   the ways to extract from
+    * @param key
+    *   the key of the tag to match
+    * @param specificValues
+    *   only match values within the Set when given otherwise match all
+    */
+  def extractClosedWays(
+      ways: List[Way],
+      key: String,
+      specificValues: Option[Set[String]] = None
+  ): List[ClosedWay] = {
+    specificValues match {
+      case Some(values) =>
+        ways.collect {
+          case way: ClosedWay if way.containsKeyValuePair(key, values) =>
+            way
+        }
+      case None =>
+        ways.collect {
+          case way: ClosedWay if way.tags.contains(KeyUtils.BUILDING) => way
+        }
+    }
+  }
+
+  /** Convenience method to extract highways from a list of ways
+    *
+    * @param ways
+    *   the ways to extract from
+    * @param specificValues
+    *   only match values within the Set when given otherwise match all
+    */
+  def extractHighways(
+      ways: List[Way],
+      specificValues: Option[Set[String]]
+  ): List[Way] = {
+    extractOpenWays(ways, KeyUtils.HIGHWAY, specificValues)
+  }
+
+  /** Extract open ways via their tags.
+    *
+    * @param ways
+    *   the ways to extract from
+    * @param key
+    *   the key of the tag to match
+    * @param specificValues
+    *   only match values within the Set when given otherwise match all
+    */
+  def extractOpenWays(
+      ways: List[Way],
+      key: String,
+      specificValues: Option[Set[String]] = None
+  ): List[Way] = {
+    specificValues match {
+      case Some(values) =>
+        ways.filter(way => way.containsKeyValuePair(key, values))
+      case None => ways.filter(way => way.tags.contains(key))
     }
   }
 
