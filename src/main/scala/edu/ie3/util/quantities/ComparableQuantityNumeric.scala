bba2ec0ae033ef857bd451ae19a2a30606776926
staudtMarius <marius.staudt@tu-dortmund.de>
2026-07-23 08:41:13 +0200

    Fixing small issues.

--- src/main/scala/edu/ie3/util/quantities/ComparableQuantityNumeric.scala
+++ src/main/scala/edu/ie3/util/quantities/ComparableQuantityNumeric.scala
@@ -8,7 +8,7 @@ import tech.units.indriya.ComparableQuantity
 import tech.units.indriya.quantity.Quantities
 import javax.measure.{Quantity, Unit}
 
-// Numeric Implementation for Quantities used
+// Numeric Implementation used for Quantities
 final class ComparableQuantityNumeric[Q <: Quantity[Q]](unit: Unit[Q])
     extends Numeric[ComparableQuantity[Q]] {
 
